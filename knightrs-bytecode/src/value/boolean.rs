dfac471d27db871a906b6c292e5da1eef921adb7
SamW <mail@sampersand.me>
2024-12-02 16:16:42 -0800

    cleanup boolean

--- knightrs-bytecode/src/value/boolean.rs
+++ knightrs-bytecode/src/value/boolean.rs
@@ -32,6 +32,7 @@ impl ToInteger for Boolean {
 	/// Returns `1` for true and `0` for false.
 	#[inline]
 	fn to_integer(&self, _: &mut Environment) -> crate::Result<Integer> {
+		// COMPLIANCE: Both `0` and `1` are always valid integers.
 		Ok(Integer::new_unvalidated(*self as i64))
 	}
 }
@@ -40,6 +41,7 @@ impl ToList for Boolean {
 	/// Returns an empty list for `false`, and a list with just `self` if true.
 	#[inline]
 	fn to_list(&self, _: &mut Environment) -> crate::Result<List> {
+		// static TRUE_BOX: List = List::new_unvalidated(true.into());
 		if *self {
 			Ok(List::boxed((*self).into()))
 		} else {
@@ -52,6 +54,7 @@ impl ToKString for Boolean {
 	/// Returns `"true"` for true and `"false"` for false.
 	#[inline]
 	fn to_kstring(&self, _: &mut Environment) -> crate::Result<KString> {
+		// VALIDATION: `true` and `false` are always valid strings.
 		static TRUE: &StringSlice = StringSlice::new_unvalidated("true");
 		static FALSE: &StringSlice = StringSlice::new_unvalidated("false");
 
