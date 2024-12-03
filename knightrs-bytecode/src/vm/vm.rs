f240b7c4e1d67234802ecfd0e1d3948a5f12dc30
SamW <mail@sampersand.me>
2024-12-02 21:58:17 -0800

    optimizing bytecode

--- knightrs-bytecode/src/vm/vm.rs
+++ knightrs-bytecode/src/vm/vm.rs
@@ -110,69 +110,78 @@ impl<'prog, 'src, 'path, 'env> Vm<'prog, 'src, 'path, 'env> {
 	}
 
 	pub fn run_inner(&mut self) -> crate::Result<Value> {
-		const NULL: Value = Value::Null;
+		use std::mem::MaybeUninit;
+		const NULL: MaybeUninit<Value> = MaybeUninit::uninit();
 
 		use Opcode::*;
+
+		let mut args = [NULL; Opcode::MAX_ARITY];
 
 		loop {
 			// SAFETY: all programs are well-formed, so we know the current index is in bounds.
 			let (opcode, offset) = unsafe { self.program.opcode_at(self.current_index) };
 			self.current_index += 1;
-
 			// println!("{:?}: {:?} / {:?}: {:?}", self.current_index, offset, opcode, self.vars);
-			let mut args = [NULL; Opcode::MAX_ARITY];
 
-			// TODO: do we need to reverse?
-			// debug_assert!(opcode.arity() <= self.stack.len());
-			// std::ptr::copy_nonoverlapping(
-			// 	self
-			// 		.stack
-			// 		.as_mut_ptr()
-			// 		.offset(self.stack.len() as isize - opcode.arity() as isize)
-			// 		.cast::<MaybeUninit<Value>>(),
-			// 	args.as_mut_ptr(),
-			// 	opcode.arity(),
-			// );
+			// Read arguments in
+			unsafe {
+				debug_assert!(opcode.arity() <= self.stack.len());
 
-			for idx in (0..opcode.arity()).rev() {
-				args[idx] = self.stack.pop().unwrap();
+				// // Copy arguments from the stack into the arguments buffer
+				// args.as_mut_ptr().copy_from_nonoverlapping(
+				// 	self
+				// 		.stack
+				// 		.as_mut_ptr()
+				// 		.offset(self.stack.len() as isize - opcode.arity() as isize)
+				// 		.cast(),
+				// 	opcode.arity(),
+				// );
+
+				// Pop the arguments off the stack.
+				self.stack.set_len(self.stack.len() - opcode.arity());
 			}
 
+			let args = self.stack.spare_capacity_mut();
+
 			macro_rules! arg {
+				(& $idx:expr) => {{
+					let idx = $idx;
+					debug_assert!(idx < opcode.arity());
+					unsafe { args[idx].assume_init_ref() }
+				}};
 				($idx:expr) => {{
 					let idx = $idx;
 					debug_assert!(idx < opcode.arity());
-					&args[idx]
+					unsafe { args[idx].assume_init_read() }
 				}};
 			}
 
-			match opcode {
+			// NOTE: ALL OPCODES MUST ALWAYS EXTRACT THEIR ARGUMENTS EXACTLY ONCE FROM `args`,
+			// else memory issues will crop up (such as memory leaks or double reads).
+			let value = match opcode {
 				// Builtins
-				PushConstant => {
-					self.stack.push(unsafe { self.program.constant_at(offset) }.clone());
-				}
+				PushConstant => unsafe { self.program.constant_at(offset) }.clone(),
 
 				Jump => {
 					self.current_index = offset;
+					continue;
 				}
 
 				JumpIfTrue => {
 					if arg![0].to_boolean(self.env)? {
 						self.current_index = offset;
 					}
+					continue;
 				}
 
 				JumpIfFalse => {
 					if !arg![0].to_boolean(self.env)? {
 						self.current_index = offset;
 					}
+					continue;
 				}
 
-				GetVar => {
-					self
-						.stack
-						.push(self.vars[offset].as_ref().expect("todo: UndefinedVariable").clone());
-				}
+				GetVar => self.vars[offset].as_ref().expect("todo: UndefinedVariable").clone(),
 
 				SetVar => {
 					let value = self.stack.last().unwrap().clone();
@@ -184,29 +193,31 @@ impl<'prog, 'src, 'path, 'env> Vm<'prog, 'src, 'path, 'env> {
 					}
 
 					self.vars[offset] = Some(value);
+					continue;
 				}
 
 				SetVarPop => todo!(), //self.vars[offset] = arg![0].clone(),
 
 				// Arity 0
-				Prompt => self.stack.push(self.env.prompt()?.map(Value::from).unwrap_or_default()),
-				Random => self.stack.push(self.env.random()?.into()),
-				Dup => self.stack.push(self.stack.last().unwrap().clone()),
+				Prompt => self.env.prompt()?.map(Value::from).unwrap_or_default(),
+				Random => self.env.random()?.into(),
+				Dup => self.stack.last().unwrap().clone(),
 				Return => return Ok(self.stack.pop().unwrap()),
 
 				// Arity 1
 				Call => {
 					let result = arg![0].kn_call(self)?;
-					self.stack.push(result);
+					result
 				}
 				Quit => {
 					let status = arg![0].to_integer(self.env)?;
 					let status = i32::try_from(status.inner()).expect("todo: out of bounds for i32");
 					self.env.quit(status)?;
+					unreachable!()
 				}
 				Dump => {
 					arg![0].kn_dump(self.env)?;
-					self.stack.push(arg![0].clone());
+					arg![0].clone()
 				}
 				Output => {
 					use std::io::Write;
@@ -222,16 +233,16 @@ impl<'prog, 'src, 'path, 'env> Vm<'prog, 'src, 'path, 'env> {
 					}
 					.map_err(|err| Error::IoError { func: "OUTPUT", err })?;
 
-					self.stack.push(Value::Null);
+					Value::Null
 				}
-				Length => self.stack.push(arg![0].kn_length(self.env)?.into()),
-				Not => self.stack.push((!arg![0].to_boolean(self.env)?).into()),
-				Negate => self.stack.push(arg![0].kn_negate(self.env)?.into()),
-				Ascii => self.stack.push(arg![0].kn_ascii(self.env)?),
-				Box => self.stack.push(List::boxed(arg![0].clone()).into()),
-				Head => self.stack.push(arg![0].kn_head(self.env)?),
-				Tail => self.stack.push(arg![0].kn_tail(self.env)?),
-				Pop => { /* do nothing, the arity already popped */ }
+				Length => arg![0].kn_length(self.env)?.into(),
+				Not => (!arg![0].to_boolean(self.env)?).into(),
+				Negate => arg![0].kn_negate(self.env)?.into(),
+				Ascii => arg![0].kn_ascii(self.env)?,
+				Box => List::boxed(arg![0].clone()).into(),
+				Head => arg![0].kn_head(self.env)?,
+				Tail => arg![0].kn_tail(self.env)?,
+				Pop => continue, /* do nothing, the arity already popped */
 
 				// TODO: the `vm` evals in its entirely own vm, which isnt what we wnat
 				#[cfg(feature = "extensions")]
@@ -239,30 +250,27 @@ impl<'prog, 'src, 'path, 'env> Vm<'prog, 'src, 'path, 'env> {
 					let program = arg![0].to_kstring(self.env)?;
 					let mut parser = crate::parser::Parser::new(&mut self.env, None, program.as_str())?;
 					let program = parser.parse_program()?;
-					self.stack.push(Vm::new(&program, self.env).run_entire_program()?);
+					Vm::new(&program, self.env).run_entire_program()?
 				}
 
 				// Arity 2
-				Add => self.stack.push(arg![0].kn_plus(arg![1], self.env)?),
-				Sub => self.stack.push(arg![0].kn_minus(arg![1], self.env)?),
-				Mul => self.stack.push(arg![0].kn_asterisk(arg![1], self.env)?),
-				Div => self.stack.push(arg![0].kn_slash(arg![1], self.env)?),
-				Mod => self.stack.push(arg![0].kn_percent(arg![1], self.env)?),
-				Pow => self.stack.push(arg![0].kn_caret(arg![1], self.env)?),
-				Lth => self
-					.stack
-					.push((arg![0].kn_compare(arg![1], "<", self.env)? == Ordering::Less).into()),
-				Gth => self
-					.stack
-					.push((arg![0].kn_compare(arg![1], ">", self.env)? == Ordering::Greater).into()),
-				Eql => self.stack.push((arg![0].kn_equals(arg![1], self.env)?).into()),
+				Add => arg![0].kn_plus(&arg![1], self.env)?,
+				Sub => arg![0].kn_minus(&arg![1], self.env)?,
+				Mul => arg![0].kn_asterisk(&arg![1], self.env)?,
+				Div => arg![0].kn_slash(&arg![1], self.env)?,
+				Mod => arg![0].kn_percent(&arg![1], self.env)?,
+				Pow => arg![0].kn_caret(&arg![1], self.env)?,
+				Lth => (arg![0].kn_compare(&arg![1], "<", self.env)? == Ordering::Less).into(),
+				Gth => (arg![0].kn_compare(&arg![1], ">", self.env)? == Ordering::Greater).into(),
+				Eql => (arg![0].kn_equals(&arg![1], self.env)?).into(),
 
 				// Arity 3
-				Get => self.stack.push(arg![0].kn_get(arg![1], arg![2], self.env)?),
+				Get => arg![0].kn_get(&arg![1], &arg![2], self.env)?,
 
 				// Arity 4
-				Set => self.stack.push(arg![0].kn_set(arg![1], arg![2], arg![3], self.env)?),
-			}
+				Set => arg![0].kn_set(&arg![1], &arg![2], &arg![3], self.env)?,
+			};
+			self.stack.push(value);
 		}
 	}
 }
